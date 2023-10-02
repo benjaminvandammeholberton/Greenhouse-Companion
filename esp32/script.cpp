1c55c950c3532a56dfc01b4866f84e2c81fee1fa
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-10-02 22:46:01 +0200

    connecting esp32 to api

---
+++ esp32/script.cpp
@@ -0,0 +1,121 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ArduinoJson.h>
+#include <HTTPClient.h>
+
+const char *ssid = "TP-Link_1E71";
+const char *password = "91319418";
+const char *serverUrl = "http://192.168.1.104:5001/sensors";
+const char *esp32Name = "ESP32";
+
+// GPIO PINS
+#define SMART_PLUG_1 26
+#define SMART_PLUG_2 27
+#define SMART_PLUG_3 14
+#define SMART_PLUG_4 12
+
+unsigned long setMillis[4] = {0};
+unsigned long currentMillis[4] = {0};
+bool timerActive[4] = {false};
+int smartPlugPins[] = {
+    SMART_PLUG_1,
+    SMART_PLUG_2,
+    SMART_PLUG_3,
+    SMART_PLUG_4};
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // Connect to Wi-Fi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
+
+    // Initialize smart plug pins
+    pinMode(SMART_PLUG_1, OUTPUT);
+    pinMode(SMART_PLUG_2, OUTPUT);
+    pinMode(SMART_PLUG_3, OUTPUT);
+    pinMode(SMART_PLUG_4, OUTPUT);
+}
+
+void loop()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (timerActive[i] && (millis() - currentMillis[i] >= setMillis[i]))
+        {
+            // Time is due, turn off the smart plug
+            digitalWrite(smartPlugPins[i], LOW);
+            timerActive[i] = false;
+        }
+    }
+
+    // Read values from sensors
+
+    // Create a JSON document
+    StaticJsonDocument<200> jsonDocument;
+    jsonDocument["air_temperature"] = "24";
+    jsonDocument["air_humidity"] = "55";
+    jsonDocument["luminosity"] = "3500";
+
+    // Serialize the JSON document to a string
+    String jsonString;
+    serializeJson(jsonDocument, jsonString);
+    Serial.print("Values sent: ");
+    Serial.println(jsonString);
+
+    // Create an HTTPClient object
+    HTTPClient http;
+
+    // Send the POST request
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+    int httpCode = http.POST(jsonString);
+
+    if (httpCode > 0)
+    {
+        String response = http.getString();
+        Serial.print("Action required: ");
+        Serial.println(response);
+
+        // Parse the JSON response
+        DynamicJsonDocument jsonDoc(400);
+        DeserializationError jsonResponse = deserializeJson(jsonDoc, response);
+
+        if (!jsonResponse)
+        {
+            int smartPlugTimers[] = {
+                jsonDoc["smart_plug_1_timer"],
+                jsonDoc["smart_plug_2_timer"],
+                jsonDoc["smart_plug_3_timer"],
+                jsonDoc["smart_plug_4_timer"]};
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (smartPlugTimers[i] != 0)
+                {
+                    // Set the timer and activate the smart plug
+                    setMillis[i] = smartPlugTimers[i];
+                    currentMillis[i] = millis();
+                    timerActive[i] = true;
+                    digitalWrite(smartPlugPins[i], HIGH);
+                }
+            }
+        }
+        else
+        {
+            Serial.println("Error parsing JSON");
+        }
+    }
+    else
+    {
+        Serial.println("Error on HTTP request");
+    }
+    http.end();
+    delay(5000);
+}
