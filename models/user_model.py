40865977ab91de7d4c28ae750964d256968bb1b3
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-10-02 22:46:40 +0200

    update the model to handle users

--- models/user_model.py
+++ models/user_model.py
@@ -1,9 +1,11 @@
 from . import db
 import uuid
 from models.base_model import BaseModel
-
+from flask_sqlalchemy import SQLAlchemy
 
 class UserModel(BaseModel, db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80))
     admin = db.Column(db.Boolean)
+
+    areas = db.relationship('AreaModel', backref='user', lazy=True)
